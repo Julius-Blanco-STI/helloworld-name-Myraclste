9e7bc6decf78f9894b774421955e011f6795974d
Myraclste <58575647+Myraclste@users.noreply.github.com>
2019-12-06 09:11:25 +0800

    Add files via upload

---
+++ Main1.java
@@ -0,0 +1,18 @@
+
+package main1;
+
+public class Main1 {
+
+    
+    public static void main(String[] args) {
+        String name = "Myra Celeste";
+        String Age = "18 yrs old";
+        String Address = "Dona Nicasia Brgy. Comm. Quezon City";
+        
+        System.out.println("Hello World");
+		System.out.println("What's your name" + name);
+        System.out.println( "How old are you?" + Age);
+        System.out.println(" What's your address" + Address);
+    }
+    }
+    
