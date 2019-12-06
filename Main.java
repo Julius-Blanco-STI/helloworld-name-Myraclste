1a213ed7006ed7432235fe49bbd3fc15b59c1acc
Myraclste <58575647+Myraclste@users.noreply.github.com>
2019-12-06 08:35:44 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
+
+package main;
+
 public class Main {
 
-  public static void main(String[] args) {
-    System.out.println("Hello World");
-  }
-
+    public static void main(String[] args) {
+        String name = "Myra Celeste";
+        System.out.println("Hello World" + name);
+    }
+    
 }
